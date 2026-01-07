68040f032c9dfe8f8ced0c39dc086cb0509297cc
dmose <100089505+janusdotai@users.noreply.github.com>
2026-01-07 12:39:10 -0500

    tested yaml input

--- tests/test_sandbox.py
+++ tests/test_sandbox.py
@@ -5,6 +5,8 @@ import affinetes as af_env
 import asyncio
 import logging
 
+import yaml
+
 logger = logging.getLogger(__name__)
 
 PARENT_DIR = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
@@ -47,6 +49,9 @@ async def test_calculator_env():
     assert result["success"] == True
 
 
+
+
+
 @pytest.mark.asyncio
 async def test_bitrecs_eval():
     env = af_env.load_env(
@@ -79,6 +84,46 @@ async def test_bitrecs_eval():
         result = response.json()
     
     print(f"Score: {result['score']}")
+    
+    # Cleanup
+    await env.cleanup()
+
+
+@pytest.mark.asyncio
+async def test_bitrecs_eval_yaml():
+    
+    env = af_env.load_env(
+        image="ghcr.io/bitrecs/bitrecs-evals:main",
+        env_vars={"OPENROUTER_API_KEY": os.environ.get("OPENROUTER_API_KEY")}, 
+        mode="docker",
+        host_network=True,
+        cleanup=False,
+        force_recreate=True,
+        host_port=8081,
+        pull=True
+    )
+    
+    assert env is not None
+    logger.info("Loaded Docker environment successfully")
+
+    yaml_file_path = os.path.join(PARENT_DIR, "miner", "miner_input.yaml")
+    with open(yaml_file_path, "r") as f:
+        miner_input_data = yaml.safe_load(f)
+    yaml_content = yaml.dump(miner_input_data)
+    logger.info(f"Loaded YAML content from : {yaml_file_path}")
+    
+    timeout = (30, 600)    
+    async with httpx.AsyncClient(timeout=timeout) as client:
+        response = await client.post(
+            "http://localhost:8081/evaluate",
+            json={"yaml_content": yaml_content},
+            headers={"Content-Type": "application/json"}
+        )
+        logger.info(f"Received response: {response.text}")
+        response.raise_for_status()
+        result = response.json()
+    
+    print(f"Evaluation Result: {result}")
     
     # Cleanup
     await env.cleanup()
