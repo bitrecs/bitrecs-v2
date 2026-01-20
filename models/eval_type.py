dd17727ad7ef968a8aaa52411b46ea951da0ec40
dmose <100089505+janusdotai@users.noreply.github.com>
2026-01-20 18:20:38 -0500

    adding eval

--- models/eval_type.py
+++ models/eval_type.py
@@ -3,6 +3,7 @@ from enum import Enum
 
 
 class BitrecsEvaluationType(str, Enum):
+    BITRECS_SAFE_DAILY = "bitrecs_safe_daily"
     BITRECS_BASIC_DAILY = "bitrecs_basic_daily"
     BITRECS_PROMPT_DAILY = "bitrecs_prompt_daily"
     BITRECS_REASON_DAILY = "bitrecs_reason_daily"
