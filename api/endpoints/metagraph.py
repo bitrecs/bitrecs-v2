c8a54e96e4cbbe6d30bde410b93352c4d5f0e2d3
dmose <100089505+janusdotai@users.noreply.github.com>
2026-01-31 12:49:14 -0500

    sync

--- api/endpoints/metagraph.py
+++ api/endpoints/metagraph.py
@@ -1,10 +1,7 @@
 import time
 import utils.logger as logger
 from fastapi import APIRouter, Request
-from utils.subtensor import get_subtensor
-from utils.ttl import ttl_cache
 from api.utils.limiter import limiter
-
 from api.snapshot import metagraph_snapshot
 
 router = APIRouter()
@@ -13,14 +10,12 @@ router = APIRouter()
 @router.get("/info")
 @limiter.limit("60/minute")
 #@ttl_cache(ttl_seconds=15) # 15 seconds
-async def info(request: Request):
-    #st = await get_subtensor()    
-    #current_block = await st.get_current_block()     
+async def info(request: Request):    
 
     logger.debug(f"Metagraph snapshot contains {len(metagraph_snapshot['nodes'])} nodes")
-    logger.debug(f"{metagraph_snapshot}")     
-    for hotkey, node_info in metagraph_snapshot["nodes"].items():
-        logger.debug(f"Node {hotkey}: {node_info}")
+    # logger.debug(f"{metagraph_snapshot}")     
+    # for hotkey, node_info in metagraph_snapshot["nodes"].items():
+    #     logger.debug(f"Node {hotkey}: {node_info}")
 
     if not metagraph_snapshot["nodes"]:
         return {"error": "Metagraph data not yet available", "timestamp": int(time.time())}
