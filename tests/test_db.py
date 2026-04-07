adb9ca890fa14e1e8fac20d22c95e24f493aa3dd
dmose <100089505+janusdotai@users.noreply.github.com>
2026-04-07 12:28:19 -0400

    testing banned hotkeys

--- tests/test_db.py
+++ tests/test_db.py
@@ -3,6 +3,7 @@ import secrets
 from datetime import datetime, timezone
 from uuid import UUID
 from models.inference_report import InferenceReport
+from queries.banned_hotkey import add_banned_hotkey
 from queries.inference import insert_inference
 from queries.system_enabled import get_system_enabled
 from utils.database import check_database_health
@@ -83,3 +84,33 @@ async def test_insert_inference_report():
     )
     assert inference_id is not None, "Expected a valid inference ID to be returned after insertion"
 
+
+@pytest.mark.asyncio
+@pytest.mark.usefixtures("db_setup")
+async def test_add_banned_hotkey():
+    from queries.banned_hotkey import add_banned_hotkey, get_banned_hotkey
+    miner_hotkey = "test_banned_hotkey"
+    banned_reason = "Testing ban functionality"
+    banned_hotkey = await add_banned_hotkey(miner_hotkey, banned_reason)
+    assert banned_hotkey is True, "Expected the banned hotkey to be added successfully"    
+
+    # Verify that the banned hotkey can be retrieved
+    retrieved_hotkey = await get_banned_hotkey(miner_hotkey)
+    assert retrieved_hotkey is not None, "Expected to retrieve the banned hotkey after insertion"
+    assert retrieved_hotkey.miner_hotkey == miner_hotkey, "Expected the retrieved miner hotkey to match the input value"
+    assert retrieved_hotkey.banned_reason == banned_reason, "Expected the retrieved banned reason to match the input value"
+    
+
+@pytest.mark.asyncio
+@pytest.mark.usefixtures("db_setup")
+async def test_load_banned_hotkeys():
+    t = ['5FCh2LAXpRWzf93Ekx7pqUmokG8BHH16dX6AxJV3ptGJzPr6', '5FNL6e4JsB3ZPUGk1x1izK1xnTWsZDZrVF6WaRp1gNpoTvsM', '5ChvSkb7Gfy7pypH5Cxo9QP2DMEJ88kwFnZvnCwESD66KVzD', '5C8KiUpE5SYwsAX1pPDbqKdLFDhGeE6JpAN4DHcYfwwvShAF', '5FGvuwULd6y5Y9L18eNEuD6M2iLF1Zst7TUwNfmJUcziiiJt', ]
+    count = 0
+    for hotkey in t:
+        banned_reason = "Testing bulk ban functionality"
+        banned_hotkey = await add_banned_hotkey(hotkey, banned_reason)
+        assert banned_hotkey is True, f"Expected the hotkey {hotkey} to be added to the ban list successfully"
+        count += 1
+        if count > 5:
+            break
+    print(f"Successfully added {count} hotkeys to the ban list")
